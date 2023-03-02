e4ddb1aa5d269b745b817b53d09476b58d6416f0
Lyndon Panton <LyndonPanton@gmail.com>
2023-03-02 23:39:53 +0000

    Imported supplied starter files

---
+++ ProgrammingAssignment2/ProgrammingAssignment2/MutualFund.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// A mutual fund
+    /// </summary>
+    public class MutualFund : InvestmentAccount
+    {
+        const float ServiceChargePercent = 0.01f;
+
+        #region Contructor
+
+
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Updates the balance by adding 6%
+        /// </summary>
+        public override void UpdateBalance()
+        {
+            // delete code below and replace with your code
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Provides balance with account type caption
+        /// </summary>
+        /// <returns>balance with caption</returns>
+        public override string ToString()
+        {
+            return "Mutual Fund Balance: " + balance;
+        }
+
+        #endregion
+    }
+}
